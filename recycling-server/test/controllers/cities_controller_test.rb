d0e7151cb15e422bef5dde6d2b565d4562d2e5d6
zhzhang2012 <zhzhangtony@gmail.com>
2017-03-01 14:49:01 -0800

    Disable auto-generated tests

--- recycling-server/test/controllers/cities_controller_test.rb
+++ recycling-server/test/controllers/cities_controller_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class CitiesControllerTest < ActionDispatch::IntegrationTest
   test "should get list" do
-    get cities_list_url
-    assert_response :success
+    # get cities_list_url
+    # assert_response :success
   end
 
   test "should get show" do
-    get cities_show_url
-    assert_response :success
+    # get cities_show_url
+    # assert_response :success
   end
 
 end
