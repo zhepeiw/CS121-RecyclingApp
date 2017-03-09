7128213675468fac1f8efe0e2f47c61cbcbb3e33
zhzhang2012 <zhzhangtony@gmail.com>
2017-03-09 01:53:28 -0800

    Build cities api endpoints

--- recycling-server/config/routes.rb
+++ recycling-server/config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   resources :cities
 
   root 'welcome#index'
+
+  # API V1
+  get 'api/v1/cities',                   action: :get_cities,              controller: 'cities_api'
+  get 'api/v1/cities/:id',               action: :get_city_by_id,          controller: 'cities_api'
+  get 'api/v1/cities/:id/categories',    action: :get_categories_by_id,    controller: 'cities_api'
+  get 'api/v1/cities/:id/subcategories', action: :get_subcategories_by_id, controller: 'cities_api'
+  get 'api/v1/cities/:id/facilities',    action: :get_facilities,          controller: 'cities_api'
+
 end
