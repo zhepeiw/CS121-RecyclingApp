e5548044a7afa7d3724e5f9878eb66184b1b2eeb
zhzhang2012 <zhzhangtony@gmail.com>
2017-03-20 02:07:30 -0700

    Try another experiment

--- recycling-server/config/initializers/elasticsearch.rb
+++ recycling-server/config/initializers/elasticsearch.rb
@@ -1,13 +1,14 @@
 require 'elasticsearch'
 
-config = {
-    host: "http://search:9200",
-    user: "elastic",
-    password: "changeme",
-}
+config = {}
 
-if File.exists?("config/elasticsearch.yml")
-  config.merge!(YAML.load_file("config/elasticsearch.yml")[Rails.env].symbolize_keys)
+if File.exists?('config/elasticsearch.yml')
+  config.merge!(YAML.load_file('config/elasticsearch.yml')[Rails.env].symbolize_keys)
+  config[:host] = "http://#{config[:hostname]}:#{config[:port]}"
+else
+  config[:host] = 'http://search:9200'
+  config[:user] = 'elastic'
+  config[:password] = 'changeme'
 end
 
 Elasticsearch::Model.client = Elasticsearch::Client.new(config)
