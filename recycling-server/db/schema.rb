b04da04b9732e6994a2abc820615c56dc5685d2e
Haunter17 <haunterhacker17@gmail.com>
2017-03-19 22:18:39 -0700

    Handling merge #15

--- recycling-server/db/schema.rb
+++ recycling-server/db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20170307100614) do
 
-  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "name",                        null: false
     t.text     "description",   limit: 65535
     t.string   "image_link"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20170307100614) do
     t.index ["name"], name: "index_categories_on_name", unique: true, using: :btree
   end
 
-  create_table "cities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "cities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "name",                      null: false
     t.string   "state",                     null: false
     t.string   "zipcode",                   null: false
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20170307100614) do
     t.index ["name"], name: "index_cities_on_name", unique: true, using: :btree
   end
 
-  create_table "facilities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "facilities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "name",           null: false
     t.string   "street_address"
     t.string   "zipcode"
@@ -44,25 +44,28 @@ ActiveRecord::Schema.define(version: 20170307100614) do
     t.index ["street_address"], name: "index_facilities_on_street_address", unique: true, using: :btree
   end
 
-  create_table "facility_recycles", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "facility_id", null: false
-    t.integer "category_id", null: false
-    t.index ["facility_id", "category_id"], name: "index_facility_recycles_on_facility_id_and_category_id", unique: true, using: :btree
+  create_table "facility_recycles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer "facilities_id"
+    t.integer "categories_id"
+    t.index ["categories_id"], name: "index_facility_recycles_on_categories_id", using: :btree
+    t.index ["facilities_id"], name: "index_facility_recycles_on_facilities_id", using: :btree
   end
 
-  create_table "has_facilities", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "city_id",     null: false
-    t.integer "facility_id", null: false
-    t.index ["city_id", "facility_id"], name: "index_has_facilities_on_city_id_and_facility_id", unique: true, using: :btree
+  create_table "has_facilities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer "cities_id"
+    t.integer "facilities_id"
+    t.index ["cities_id"], name: "index_has_facilities_on_cities_id", using: :btree
+    t.index ["facilities_id"], name: "index_has_facilities_on_facilities_id", using: :btree
   end
 
-  create_table "recycles", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "city_id",        null: false
-    t.integer "subcategory_id", null: false
-    t.index ["city_id", "subcategory_id"], name: "index_recycles_on_city_id_and_subcategory_id", unique: true, using: :btree
+  create_table "recycles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer "cities_id"
+    t.integer "subcategories_id"
+    t.index ["cities_id"], name: "index_recycles_on_cities_id", using: :btree
+    t.index ["subcategories_id"], name: "index_recycles_on_subcategories_id", using: :btree
   end
 
-  create_table "subcategories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "subcategories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "name",                      null: false
     t.text     "description", limit: 65535
     t.string   "image_link"
@@ -73,5 +76,11 @@ ActiveRecord::Schema.define(version: 20170307100614) do
     t.index ["name"], name: "index_subcategories_on_name", unique: true, using: :btree
   end
 
+  add_foreign_key "facility_recycles", "categories", column: "categories_id"
+  add_foreign_key "facility_recycles", "facilities", column: "facilities_id"
+  add_foreign_key "has_facilities", "cities", column: "cities_id"
+  add_foreign_key "has_facilities", "facilities", column: "facilities_id"
+  add_foreign_key "recycles", "cities", column: "cities_id"
+  add_foreign_key "recycles", "subcategories", column: "subcategories_id"
   add_foreign_key "subcategories", "categories"
 end
