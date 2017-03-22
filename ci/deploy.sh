0a21dd7c0e21aa1f5380df71fe2487f81d0bcf0b
zhzhang2012 <zhzhangtony@gmail.com>
2017-03-22 14:30:25 -0700

    Fix more production limit setting

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -33,12 +33,11 @@ construct_prod_configs() {
       < $1 > $2
 }
 
-printf "Start updating assets...\n"
-# Note(tony): Temporarily disable S3 upload to avoid costs
-# Note2(tony): can't do this since production will crash. Fuck rails.
-aws s3 rm s3://$ASSET_BUCKET/assets --recursive
-aws s3 cp --recursive $ASSET_DIR s3://$ASSET_BUCKET/assets --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-printf "Finished updating assets\n"
+## Note(tony): Temporarily disable S3 upload to avoid costs, Wait for next month
+## Note2(tony): can't do this since production will crash. Fuck rails.
+#aws s3 rm s3://$ASSET_BUCKET/assets --recursive
+#aws s3 cp --recursive $ASSET_DIR s3://$ASSET_BUCKET/assets --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+#printf "Finished updating assets\n"
 
 # Set working directory for the script to /ci
 cd "$(dirname "$0")"
