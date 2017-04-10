5074ad9fc08e5f4f2816db634d1fdcbfeff0ac06
Cam Zhou <wzhou@hmc.edu>
2017-04-10 02:08:31 -0700

    Write additional fields to db

--- recycling-server/config/routes.rb
+++ recycling-server/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   get 'users/:id', action: :show, controller: 'profile'
 
   get 'developers/index'
